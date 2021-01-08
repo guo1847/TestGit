7d07187d1bb32bf9ef9ba090aeeff3cbd2a5fc9a
emma <guoying1847@163.com>
2021-01-08 16:33:07 +0800

    commit

---
+++ src/test/java/com/cn/HelloTest.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
